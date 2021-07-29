c583f3719e0d38beb9443891f9fc6395c28b8725
davidleegriffin <davidleegriffin@yahoo.com>
2021-07-28 21:17:08 -0500

    data fix

--- blackjack-vegas-rules/src/components/Dealer.js
+++ blackjack-vegas-rules/src/components/Dealer.js
@@ -3,14 +3,14 @@ import React, { useState, useEffect } from 'react';
 function Dealer() {
     const deckId = localStorage.getItem("deck");
     const [dealerCards, setDealerCards] = useState([]);
-    
+
     useEffect(() => {
         const dealDealerCards = async () => {
             let dealer_cards = [];
             const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
             const data = await response.json();
             console.log('data', data);
-            dealer_cards.push(data.cards[0]);
+            dealer_cards.push(data.cards);
             // dealer_cards.push(data.cards[1]);
             setDealerCards(dealer_cards)
         };
