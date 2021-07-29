ec582f94431d4cd7b4fcba2dbf03f3092a1f07c5
davidleegriffin <davidleegriffin@yahoo.com>
2021-07-28 22:08:03 -0500

    hit me fuctionality

--- blackjack-vegas-rules/src/components/Player.js
+++ blackjack-vegas-rules/src/components/Player.js
@@ -3,24 +3,24 @@ import React, {useEffect, useState} from 'react';
 function Player() {
     const deckId = localStorage.getItem("deck");
     let playerNumCards = 2;
-    
+
     const [playerCards, setPlayerCards] = useState([]);
 
     // function addPlayerCard() {
     //     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     //             .then(response => response.json())
-    //             // .then(data=> console.log(data?.cards[0]))      
+    //             // .then(data=> console.log(data?.cards[0]))
     //             .then(data => playerCards.push(data.cards[0]));
     // }
     // addPlayerCard();
-    
+
     useEffect(() => {
         const dealPlayerCards = async () => {
             let player_cards = [];
             const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
             const data = await response.json();
             player_cards.push(data.cards[0]);
-            player_cards.push(data.cards[1]);
+            // player_cards.push(data.cards[1]);
             setPlayerCards((playerCards) => [...playerCards, player_cards])
         };
         dealPlayerCards();
@@ -29,9 +29,15 @@ function Player() {
 
     console.log('playerCards', playerCards);
 
-    function hitPlayer() {
+    async function hitPlayer() {
         playerNumCards++;
         console.log('hitPlayer', playerNumCards);
+        let player_cards = [];
+        const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+        const data = await response.json();
+        player_cards.push(data.cards[0]);
+        // player_cards.push(data.cards[1]);
+        setPlayerCards((playerCards) => [...playerCards, player_cards])
     }
 
     return (
