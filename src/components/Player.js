880574a9f4291679ed8ff486adfa38d9cb93d8a7
davidleegriffin <davidleegriffin@yahoo.com>
2021-08-02 12:51:14 -0500

    meeting

--- src/components/Player.js
+++ src/components/Player.js
@@ -40,7 +40,7 @@ function Player(props) {
             let player_cards = [];
             const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
             const data = await response.json();
-            // console.log('playerData', data);
+            console.log('playerData', data);
             // console.log('playerDeckId', deckId);
             // console.log('playerReduxDeckId', reduxDeckId);
             // console.log(deckId === reduxDeckId);
